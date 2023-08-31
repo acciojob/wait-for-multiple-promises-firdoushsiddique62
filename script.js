23d34a587b35b7c0b23bdc5006d03ebd68badf3d
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-31 19:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
-//your JS code here. If required.
-//your JS code here. If required.
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+ //your JS code here. If required.
+let output = document.getElementById("output");
 
-function populateTable(data) {
-  const table = document.getElementById('resultTable');
-  const loadingRow = document.getElementById('loadingRow');
-  table.removeChild(loadingRow);
+//Creating a default table with Loading...
+let tableRow = document.createElement("tr");
+tableRow.id = "loading";
+let tableCol = document.createElement("td");
+tableCol.colSpan = 2;
 
-  data.forEach((item, index) => {
-    const newRow = table.insertRow();
-    const promiseCell = newRow.insertCell(0);
-    const timeCell = newRow.insertCell(1);
+tableCol.innerText = "Loading...";
+ tableRow.appendChild(tableCol);
 
-    promiseCell.textContent = `Promise ${index + 1}`;
-    timeCell.textContent = `${item.toFixed(3)}`;
+output.appendChild(tableRow);
+
+//Creating Promises
+
+let prom1 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 3000) + 1000;
+  setTimeout(() => {
+    resolve("Promise 1");
+  }, time);
+});
+
+let prom2 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 3000) + 1000;
+  setTimeout(() => {
+    resolve("Promise 2");
+  }, time);
+});
+
+let prom3 = new Promise((resolve) => {
+  let time = Math.floor(Math.random() * 3000) + 1000;
+  setTimeout(() => {
+    resolve("Promise 3");
+  }, time);
+});
+
+//Getting all resolved promises
+let x = Promise.all([prom1, prom2, prom3]);
+
+//Add resolved promises data into the table
+x.then((data) => {
+  output.removeChild(tableRow);
+  data.forEach((val) => {
+    console.log(val);
+    let newTableRow = document.createElement("tr");
+
+    let tableColumn_1 = document.createElement("td");
+
+    let tableColumn_2 = document.createElement("td");
+
+    tableColumn_1.innerText = `${val}`;
+
+    tableColumn_2.innerText = `${(new Date() - startTime) / 1000}`;
+
+    newTableRow.appendChild(tableColumn_1);
+    newTableRow.appendChild(tableColumn_2);
+    output.appendChild(newTableRow);
   });
-}
 
-const promises = [];
+  let newTableRow = document.createElement("tr");
 
-promises.push(
-  new Promise(resolve => {
-    const randomDelay = Math.random() * 2000 + 1000;
-    setTimeout(() => resolve(randomDelay / 1000), randomDelay);
-  })
-);
+  let tableColumn_1 = document.createElement("td");
 
-promises.push(
-  new Promise(resolve => {
-    const randomDelay = Math.random() * 2000 + 1000;
-    setTimeout(() => resolve(randomDelay / 1000), randomDelay);
-  })
-);
+  let tableColumn_2 = document.createElement("td");
 
-promises.push(
-  new Promise(resolve => {
-    const randomDelay = Math.random() * 2000 + 1000;
-    setTimeout(() => resolve(randomDelay / 1000), randomDelay);
-  })
-);
+  tableColumn_1.innerText = `Total`;
 
-Promise.all(promises)
-  .then(data => {
-    const total = data.reduce((sum, time) => sum + time, 0);
-    data.push(total);
-    populateTable(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  tableColumn_2.innerText = `${(new Date() - startTime) / 1000}`;
+
+  newTableRow.appendChild(tableColumn_1);
+  newTableRow.appendChild(tableColumn_2);
+  output.appendChild(newTableRow);
+});
+
+const startTime = new Date();
